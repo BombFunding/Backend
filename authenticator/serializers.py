f95b6d8c8eeecff21f5574ae5d051399979aa5aa
MiliAxe <miliaxe0@gmail.com>
2024-11-03 19:22:12 +0330

    Fix: enforce required fields in RegisterSerializer during user registration

--- authenticator/serializers.py
+++ authenticator/serializers.py
@@ -4,14 +4,16 @@ from .models import BaseUser
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    required_fields = ['username', 'email', 'password', 'user_type']
 
     class Meta:
         model = BaseUser
         fields = ('id', 'username', 'email', 'name', 'about_me', 'user_type', 'password')
 
     def create(self, validated_data):
-        if "user_type" not in validated_data:
-            raise serializers.ValidationError({"user_type": "This field is required."})
+        for field in self.required_fields:
+            if field not in validated_data:
+                raise serializers.ValidationError({field: f"{field.capitalize()} is required."})
         user = BaseUser.objects.create_user(**validated_data)
         user.set_password(validated_data['password'])
         user.save()
