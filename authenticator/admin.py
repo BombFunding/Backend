8e24dde7b53c95f41dd8a96873eae7b4bfbd9fa2
taraneh <taraneh.abdellahi2@gmail.com>
2024-11-14 12:29:09 +0330

    in auth/admin.py when you want to change inherited field it jumps to the baseuser

--- authenticator/admin.py
+++ authenticator/admin.py
@@ -1,38 +1,54 @@
 from django.contrib import admin
+from django.shortcuts import redirect
+from django.urls import reverse
 from .models import BaseUser, BasicUser, InvestorUser, StartupUser
 
 class BaseUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'name', 'user_type', 'is_confirmed', 'is_staff', 'is_superuser')
+    list_display = ('username', 'email', 'user_type', 'is_staff', 'is_superuser')
     search_fields = ('username', 'email')
-    list_filter = ('user_type', 'is_confirmed', 'is_staff', 'is_superuser')
+    list_filter = ('user_type', 'is_staff', 'is_superuser')
 
 class BasicUserAdmin(admin.ModelAdmin):
     list_display = ('get_username',)
-    search_fields = ('username__username',)  
+    search_fields = ('username__username',)
 
     def get_username(self, obj):
         return obj.username.username  
-    get_username.admin_order_field = 'username'  
-    get_username.short_description = 'Username'  
+    get_username.admin_order_field = 'username'
+    get_username.short_description = 'Username'
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        basic_user = self.get_object(request, object_id)
+        base_user_id = basic_user.username.id
+        return redirect(reverse('admin:authenticator_baseuser_change', args=[base_user_id]))
 
 class InvestorUserAdmin(admin.ModelAdmin):
-    list_display = ('get_username', 'page', 'categories')
-    search_fields = ('username__username',)  
-    list_filter = ('username__user_type',)
+    list_display = ('get_username',)
+    search_fields = ('username__username',)
 
     def get_username(self, obj):
         return obj.username.username  
-    get_username.admin_order_field = 'username'  
-    get_username.short_description = 'Username'  
+    get_username.admin_order_field = 'username'
+    get_username.short_description = 'Username'
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        investor_user = self.get_object(request, object_id)
+        base_user_id = investor_user.username.id
+        return redirect(reverse('admin:authenticator_baseuser_change', args=[base_user_id]))
 
 class StartupUserAdmin(admin.ModelAdmin):
     list_display = ('get_username',)
-    search_fields = ('username__username',)  
+    search_fields = ('username__username',)
 
     def get_username(self, obj):
         return obj.username.username  
-    get_username.admin_order_field = 'username'  
-    get_username.short_description = 'Username'  
+    get_username.admin_order_field = 'username'
+    get_username.short_description = 'Username'
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        startup_user = self.get_object(request, object_id)
+        base_user_id = startup_user.username.id
+        return redirect(reverse('admin:authenticator_baseuser_change', args=[base_user_id]))
 
 
 admin.site.register(BaseUser, BaseUserAdmin)
