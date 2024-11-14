52470542ebd69005dd219509aa97ef4731c54fc9
taraneh <taraneh.abdellahi2@gmail.com>
2024-11-14 17:34:04 +0330

    basic profile "own_view"+ "others_view"

--- authenticator/urls.py
+++ authenticator/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView,view_own_basic_user_profile,view_basic_user_profile
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/', view_own_basic_user_profile, name='view_own_basic_user_profile'),
+    path('profile/<str:username>/', view_basic_user_profile, name='view_basic_user_profile'),
 ]
--- authenticator/views.py
+++ authenticator/views.py
@@ -1,9 +1,17 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from django.conf import settings
+from .models import BasicUserProfile, BasicUser
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import RegisterSerializer, LoginSerializer
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from .models import BasicUserProfile
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
@@ -36,3 +44,60 @@ class LoginView(generics.CreateAPIView):
              "refresh_token": str(refresh)
              }
         , status=status.HTTP_200_OK)
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def view_own_basic_user_profile(request):
+    user = request.user  
+    try:
+        
+        basic_user_profile = BasicUserProfile.objects.get(username=user)
+
+        
+        profile_picture_url = None
+        if basic_user_profile.profile_picture:
+            profile_picture_url = request.build_absolute_uri(basic_user_profile.profile_picture.url)
+
+        return Response({
+            'basic_user_profile': {
+                'username': user.username,
+                'email': user.email,
+                'about_me': user.about_me,
+                'interests': basic_user_profile.interests,
+                'password': user.password,
+                'profile_picture': profile_picture_url  
+            }
+        }, status=status.HTTP_200_OK)
+    
+    except BasicUserProfile.DoesNotExist:
+        return Response({'detail': 'Basic user profile not found.'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def view_basic_user_profile(request, username):
+    try:
+        
+        basic_user_profile = BasicUserProfile.objects.get(username__username=username)
+        user = basic_user_profile.username
+
+        
+        profile_picture_url = None
+        if basic_user_profile.profile_picture:
+            profile_picture_url = request.build_absolute_uri(basic_user_profile.profile_picture.url)
+
+        return Response({
+            'basic_user_profile': {
+                'username': user.username,
+                'email': user.email,
+                'about_me': user.about_me,
+                'profile_picture': profile_picture_url  
+            }
+        }, status=status.HTTP_200_OK)
+
+    except BasicUserProfile.DoesNotExist:
+        return Response({'detail': 'Basic user profile not found.'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'detail': str(e)}, status=status.HTTP_400_BAD_REQUEST)
