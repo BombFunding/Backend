a01392043730625e3f0e42e522ce36a9a93221ba
taraneh <taraneh.abdellahi2@gmail.com>
2024-11-22 12:41:26 +0330

    add media path to bombfunding/urls.py

--- Bombfunding/urls.py
+++ Bombfunding/urls.py
@@ -37,3 +37,8 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('startup/', include('startup.urls')),  
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG: 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
