4660262eb0350036a5631197022f1aa70ecd4761
taraneh <taraneh.abdellahi2@gmail.com>
2024-11-29 16:56:49 +0330

    add (strtup): add startup profile card with fileds: rank, categories, visit_count, starting_date

--- startup/models.py
+++ startup/models.py
@@ -1,6 +1,32 @@
 from django.db import models
 from authenticator.models import BaseUser, StartupUser,BaseProfile
 
+class StartupProfile(models.Model):
+    base_profile = models.OneToOneField(
+        BaseProfile, on_delete=models.CASCADE, editable=False
+    )
+    startup_rank = models.IntegerField(default=0)  
+    startup_categories = models.CharField(
+        max_length=50,
+        choices=[
+            ("Technology", "Technology"),
+            ("Food", "Food"),
+            ("Beauty", "Beauty"),
+            ("Art", "Art"),
+            ("Health", "Health"),
+            ("Tourism", "Tourism"),
+            ("Education", "Education"),
+            ("Finance", "Finance"),
+        ],
+        default="Technology",
+    )  
+    startup_starting_date = models.DateField(null=True, blank=True)  
+    startup_profile_visit_count = models.PositiveIntegerField(default=0)  
+
+    def __str__(self):
+        return f"{self.base_profile.name}"
+
+
 class StartupPosition(models.Model):
     startup_profile = models.ForeignKey(BaseProfile, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
