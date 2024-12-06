af35eae596737dfeaf29c8b7a74e47c9d79878bb
taraneh <152805258+taraneh2abd@users.noreply.github.com>
2024-12-06 23:31:30 +0330

    finished merging with team

--- startup/signals.py
+++ startup/signals.py
@@ -1,13 +1,9 @@
 from django.db.models.signals import post_save
-from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from team.models import Team
 from startup.models import StartupProfile
+from team.models import Team
 
-@receiver(post_delete, sender=StartupProfile)
-def delete_team_for_startup(sender, instance, **kwargs):
-    try:
-        team = Team.objects.get(startup_profile=instance)
-        team.delete()
-    except Team.DoesNotExist:
-        pass        
+@receiver(post_save, sender=StartupProfile)
+def create_team_for_startup(sender, instance, created, **kwargs):
+    if created:
+        Team.objects.create(startup_profile=instance)
