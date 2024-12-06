7f1fdf564436ad56238ea04f251215e4e235420a
taraneh <152805258+taraneh2abd@users.noreply.github.com>
2024-12-07 00:44:39 +0330

    add all swagger details for startup

--- startup/urls.py
+++ startup/urls.py
@@ -1,13 +1,11 @@
-from django.urls import path
-from .views import StartupProfileViews, StartupPositionViews
-from django.urls import include, path
+from django.urls import path, include
+from .views import StartupProfileRetrieveView, StartupProfileUpdateView, StartupPositionCreateView, StartupPositionUpdateView, StartupPositionDeleteView
 
 urlpatterns = [
-    path('get_startup_profile/<str:username>/', StartupProfileViews.get_startup_profile, name='get-startup-profile'),
-    path('update_startup_profile/', StartupProfileViews.update_startup_profile, name='update-startup-profile'),
-    path("position/create/", StartupPositionViews.create_startup_position, name="create_startup_position"),
-    path("position/update/<int:position_id>/", StartupPositionViews.update_startup_position, name="update_startup_position"),
-    path("position/delete/<int:position_id>/", StartupPositionViews.delete_startup_position, name="delete_startup_position"),
+    path('get_startup_profile/<str:username>/', StartupProfileRetrieveView.as_view(), name='get-startup-profile'),
+    path('update_startup_profile/', StartupProfileUpdateView.as_view(), name='update-startup-profile'),
+    path("position/create/", StartupPositionCreateView.as_view(), name="create_startup_position"),
+    path("position/update/<int:position_id>/", StartupPositionUpdateView.as_view(), name="update_startup_position"),
+    path("position/delete/<int:position_id>/", StartupPositionDeleteView.as_view(), name="delete_startup_position"),
     path("profile/<int:startup_profile_id>/team/", include("team.urls")),
-
 ]
