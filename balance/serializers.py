f00ce721c5cf3174524d62f666973c76e254292b
MiliAxe <miliaxe0@gmail.com>
2024-12-12 15:04:48 +0330

    feat: add cost to position creation and fix bugs

--- balance/serializers.py
+++ balance/serializers.py
@@ -5,8 +5,9 @@ class BalanceSerializer(serializers.Serializer):
     amount = serializers.IntegerField(write_only=True)
 
     def update(self, instance, validated_data):
-        if instance.balance - validated_data.get("amount") < 0:
+        amount = validated_data.get("amount")
+        if amount < 0 and instance.balance + amount < 0:
             raise serializers.ValidationError({"error": "Insufficient balance"})
-        instance.balance = instance.balance - validated_data.get("amount")
+        instance.balance += amount
         instance.save()
         return instance
