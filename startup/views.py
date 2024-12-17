3145544e98c29a6d2031445fc0f0a3d37fe0933f
taraneh <152805258+taraneh2abd@users.noreply.github.com>
2024-12-17 23:05:05 +0330

    Fix: deleted increment_like function call in vote Class

--- startup/views.py
+++ startup/views.py
@@ -19,7 +19,7 @@ from django.utils.translation import gettext as _
 from drf_yasg.utils import swagger_auto_schema
 from balance.utils import UserBalanceMixin
 from datetime import date
-from profile_statics.models import ProfileStatics
+
 
 POSITION_CREATION_COST = 100000
 
@@ -81,8 +81,6 @@ class StartupProfileRetrieveView(mixins.RetrieveModelMixin, generics.GenericAPIV
         if request.user.username != username:
             startup_profile.startup_profile_visit_count += 1
             startup_profile.save()
-            profile_statics = ProfileStatics.objects.get(user=startup_user.username)
-            profile_statics.increment_view() 
 
         serializer = self.get_serializer(startup_profile)
         return Response({"profile": serializer.data}, status=status.HTTP_200_OK)
@@ -296,7 +294,6 @@ class VoteProfile(GenericAPIView):
             )
 
         try:
-            
             startup_profile = StartupProfile.objects.get(pk=startup_profile_id)
         except StartupProfile.DoesNotExist:
             return Response(
@@ -305,37 +302,9 @@ class VoteProfile(GenericAPIView):
             )
 
         try:
-            
             startup_vote, created = StartupVote.objects.update_or_create(
                 user=user, startup_profile=startup_profile, defaults={"vote": vote_type}
             )
-
-            
-            profile_statics, created = ProfileStatics.objects.get_or_create(
-                user=startup_profile.user
-            )
-
-            
-            today = date.today().isoformat()
-
-            
-            if vote_type == 1:
-                if today in profile_statics.likes:
-                    profile_statics.likes[today] += 1  
-                else:
-                    profile_statics.likes[today] = 1  
-
-            
-            elif vote_type == -1:
-                if today in profile_statics.likes:
-                    profile_statics.likes[today] -= 1  
-                else:
-                    profile_statics.likes[today] = -1  
-
-            
-            profile_statics.save()
-
-            
             if created:
                 return Response(
                     {"detail": "Vote added successfully."},
@@ -350,7 +319,7 @@ class VoteProfile(GenericAPIView):
                 {"detail": "You have already voted on this profile."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        
+
     @swagger_auto_schema(
         responses={
             204: "Vote removed successfully.",
