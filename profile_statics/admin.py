86a544215bfc4eb51c3b292b87d3062fc7aded32
taraneh <152805258+taraneh2abd@users.noreply.github.com>
2024-12-17 22:25:23 +0330

    create admin.py for profile_statics

--- profile_statics/admin.py
+++ profile_statics/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import ProfileStatics
+from django.utils.safestring import mark_safe
+import json
 
-# Register your models here.
+@admin.register(ProfileStatics)
+class ProfileStaticsAdmin(admin.ModelAdmin):
+    list_display = ("user", "display_likes", "display_views")  
+    search_fields = ("user__username", "user__email")  
+    list_filter = ("user__user_type",)  
+    readonly_fields = ("user", "display_likes", "display_views")  
+
+    def display_likes(self, obj):
+        likes_data = json.dumps(obj.likes, indent=2)  
+        return mark_safe(f"<pre>{likes_data}</pre>")  
+
+    def display_views(self, obj):
+        views_data = json.dumps(obj.views, indent=2)
+        return mark_safe(f"<pre>{views_data}</pre>")
+
+    display_likes.short_description = "Likes (JSON Format)"
+    display_views.short_description = "Views (JSON Format)"
