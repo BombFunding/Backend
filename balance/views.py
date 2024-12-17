e988ddf6c9defcdb78bb8e31a5a4819ee9aa045c
taraneh <152805258+taraneh2abd@users.noreply.github.com>
2024-12-18 01:19:22 +0330

    Revert the function to increase the balance by Post Request

--- balance/views.py
+++ balance/views.py
@@ -11,6 +11,37 @@ class BalanceUpdateView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     @swagger_auto_schema(
+        operation_description="Update the balance of the authenticated user",
+        request_body=BalanceSerializer,
+        responses={
+            200: openapi.Response(
+                description="Balance updated",
+                schema=openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        "message": openapi.Schema(type=openapi.TYPE_STRING),
+                        "balance": openapi.Schema(
+                            type=openapi.TYPE_INTEGER,
+                            description="Balance after upadating",
+                        ),
+                    },
+                ),
+            ),
+            400: "Bad Request",
+        },
+    )
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.update(user, serializer.validated_data)
+            return Response(
+                {"message": "balance updated", "balance": user.balance},
+                status=status.HTTP_200_OK,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
         operation_description="Get the balance of the authenticated user",
     )
     def get(self, request, *args, **kwargs):
