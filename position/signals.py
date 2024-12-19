e0829470400b85f6ca4d27b3c08ce06d46a989e2
taraneh <152805258+taraneh2abd@users.noreply.github.com>
2024-12-19 15:06:42 +0330

    Fix : trigger position signal by admin

--- position/signals.py
+++ position/signals.py
@@ -1,12 +1,13 @@
-
-
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Transaction, BaseUser, Position
+from .models import Transaction, BaseUser
 
 @receiver(post_save, sender=BaseUser)
 def create_transaction(sender, instance, created, **kwargs):
-    
+    """
+    Signal that triggers after a BaseUser instance is saved.
+    It works for both Admin and API save actions.
+    """
     if not created:
         
         last_transaction = Transaction.objects.filter(investor_user=instance).last()
