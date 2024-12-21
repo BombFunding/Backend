bca5e0fac1ed4868b80850ce508e417fd5688ab9
taraneh <152805258+taraneh2abd@users.noreply.github.com>
2024-12-22 02:45:45 +0330

    merge with last commit of feature/starboard wich was creating check-like/ api

--- profile_statics/urls.py
+++ profile_statics/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import ProfileStaticsLast7DaysView
 from .views import ProfileStaticsLast6MonthsView
+from .views import CheckProfileLikeView
 
 urlpatterns = [
     path('last-7-days/', ProfileStaticsLast7DaysView.as_view(), name='profile-statistics-last-7-days'),
     path('last-6-months/', ProfileStaticsLast6MonthsView.as_view(), name='profile-statistics-last-6-months'),
+    path('check-like/', CheckProfileLikeView.as_view(), name='check-profile-like'),
 ]
