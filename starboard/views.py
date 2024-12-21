3aa477b9bfdbcbdb0111036a853e48813b2aaa22
taraneh <152805258+taraneh2abd@users.noreply.github.com>
2024-12-21 22:33:02 +0330

    better swagger for starboard

--- starboard/views.py
+++ starboard/views.py
@@ -1,39 +1,44 @@
 from django.db.models import Sum
-from django.shortcuts import render
-from django.db import models
-from authenticator.models import BaseUser, StartupUser, BaseProfile
+from authenticator.models import BaseUser, BaseProfile
 from startup.models import Position, StartupProfile
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
 
+
+type_param = openapi.Parameter(
+    'type',
+    openapi.IN_QUERY,
+    description="Type of the top startups to retrieve: 'top_liked', 'top_visited', or 'top_funded'.",
+    type=openapi.TYPE_STRING,
+    enum=["top_liked", "top_visited", "top_funded"]  
+)
+
+
+results_per_page_param = openapi.Parameter(
+    'results_per_page',
+    openapi.IN_QUERY,
+    description="Number of results per page",
+    type=openapi.TYPE_INTEGER,
+    default=10
+)
+
+page_number_param = openapi.Parameter(
+    'page_number',
+    openapi.IN_QUERY,
+    description="Page number to retrieve",
+    type=openapi.TYPE_INTEGER,
+    default=1
+)
+
+
 @swagger_auto_schema(
-    method="post",
-    operation_description="Retrieve startups based on the specified 'type' ('top_liked', 'top_visited', 'top_funded') and pagination details.",
-    request_body=openapi.Schema(
-        type=openapi.TYPE_OBJECT,
-        properties={
-            'type': openapi.Schema(
-                type=openapi.TYPE_STRING,
-                description="Type of the top startups to retrieve: 'top_liked', 'top_visited', or 'top_funded'."
-            ),
-            'results_per_page': openapi.Schema(
-                type=openapi.TYPE_INTEGER,
-                description="Number of results per page",
-                default=10
-            ),
-            'page_number': openapi.Schema(
-                type=openapi.TYPE_INTEGER,
-                description="Page number to retrieve",
-                default=1
-            ),
-        },
-        required=['type']
-    ),
+    method="get",
+    manual_parameters=[type_param, results_per_page_param, page_number_param],
     responses={
         200: openapi.Schema(
             type=openapi.TYPE_ARRAY,
@@ -51,17 +56,16 @@ from drf_yasg import openapi
         400: "Invalid request"
     },
 )
-@api_view(["POST"])
+@api_view(["GET"])
 @permission_classes([AllowAny])
 def get_top_startups(request):
-    top_type = request.data.get("type")
-    results_per_page = request.data.get("results_per_page", 10)
-    page_number = request.data.get("page_number", 1)
+    top_type = request.GET.get("type")  
+    results_per_page = int(request.GET.get("results_per_page", 10))
+    page_number = int(request.GET.get("page_number", 1))
 
     if not top_type:
         return Response({"error": "'type' parameter is required."}, status=400)
 
-    
     start_index = (page_number - 1) * results_per_page
     end_index = start_index + results_per_page
 
@@ -112,24 +116,20 @@ def get_top_startups(request):
             })
 
     elif top_type == "top_funded":
-        positions = Position.objects.values('position_user').annotate(
-            total_funded=Sum('funded')
-        ).order_by('-total_funded')[start_index:end_index]
+        
+        funded_data = (
+            Position.objects.values('position_user__username')
+            .annotate(total_funded=Sum('funded'))
+            .order_by('-total_funded')[start_index:end_index]
+        )
 
-        for position_data in positions:
-            total_funded = position_data['total_funded']
+        for entry in funded_data:
+            username = entry['position_user__username']
+            total_funded = entry['total_funded']
             profile_picture = None
-            user_id = position_data['position_user']
 
             try:
-                user = BaseUser.objects.get(id=user_id, user_type="startup")
-            except BaseUser.DoesNotExist:
-                continue
-
-            username = user.username
-
-            try:
-                base_profile = BaseProfile.objects.get(base_user=user_id)
+                base_profile = BaseProfile.objects.get(base_user__username=username)
                 if base_profile.profile_picture:
                     profile_picture = base_profile.profile_picture.url
             except BaseProfile.DoesNotExist:
