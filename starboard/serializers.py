99f2abbca3d961034e6a64d13868e4956a5f9d0c
MiliAxe <miliaxe0@gmail.com>
2025-01-04 18:52:20 +0330

    feat: add open position field to ProjectListSerializer with utility function integration

--- starboard/serializers.py
+++ starboard/serializers.py
@@ -3,6 +3,8 @@ from project.models import Project
 from rest_framework import serializers
 from like.models import Like
 from bookmark.models import Bookmark
+from position.utils import get_open_position
+from position.serializers import PositionSerializer
 
 class ProjectListSerializer(serializers.ModelSerializer):
     visit_count = serializers.SerializerMethodField()
@@ -11,11 +13,12 @@ class ProjectListSerializer(serializers.ModelSerializer):
     is_bookmarked = serializers.SerializerMethodField()
     owner_username = serializers.CharField(source='user.username', read_only=True)
     owner_profile_picture = serializers.ImageField(source='user.profile.profile_picture', read_only=True)
-    
+    open_position = serializers.SerializerMethodField()
+
     class Meta:
         model = Project
         fields = ['id', 'name', 'image', 'description', 
-                 'subcategories', 'visit_count', 'like_count', 'is_liked', "owner_username", "owner_profile_picture", 'is_bookmarked', 'creation_date']
+                 'subcategories', 'visit_count', 'like_count', 'is_liked', "owner_username", "owner_profile_picture", 'is_bookmarked', 'creation_date', 'open_position']
 
     def get_visit_count(self, obj):
         try:
@@ -40,3 +43,9 @@ class ProjectListSerializer(serializers.ModelSerializer):
             return Bookmark.objects.filter(owner=self.context['request'].user, target=obj).exists()
         except Exception:
             return None
+
+    def get_open_position(self, obj):
+        position = get_open_position(obj)
+        if position:
+            return PositionSerializer(position).data
+        return None
