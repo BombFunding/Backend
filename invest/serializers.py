662b469678820d4dc0e44345d1fa6aeb81016a7f
Milad Zarei <miliaxe0@gmail.com>
2025-01-06 19:32:59 +0330

    feat: add project field to TransactionSerializer for enhanced transaction details

--- invest/serializers.py
+++ invest/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from .models import Transaction
+from project.serializers import ProjectSerializer
+from project.models import Project
 
 class TransactionSerializer(serializers.ModelSerializer):
     position_description = serializers.CharField(source="position.description", read_only=True)
+    project = serializers.SerializerMethodField()
 
     class Meta:
         model = Transaction
-        fields = ['id', 'investor_user', 'position', 'position_description', 'investment_amount', 'investment_date']
+        fields = ['id', 'investor_user', 'position', 'position_description', 'investment_amount', 'investment_date', 'project']
+
+    def get_project_data(self, obj):
+        return ProjectSerializer(Project.objects.get(positions=obj.position)).data
+        
 
 class TransactionHistorySerializer(serializers.ModelSerializer):
     class Meta:
