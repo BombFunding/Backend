7ab1fe97ec4fe991e58d9ebbc6068d531f8a20c6
MiliAxe <miliaxe0@gmail.com>
2025-01-23 16:23:19 +0330

    make shell environment variables override credentials.env variables

--- Bombfunding/settings.py
+++ Bombfunding/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 import environ
 
 env = environ.Env()
-environ.Env.read_env("credentials.env")
+environ.Env.read_env("credentials.env", override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
